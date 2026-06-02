51549c6f665573187044f0a11c7d6b1758109be3
srustikn02 <srustikn02@gmail.com>
2026-06-02 22:07:51 +0530

    reverting the script.js locales

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,7 +3,7 @@ import { loadArea, setConfig } from './ak.js';
 const hostnames = ['authorkit.dev'];
 
 const locales = {
-  '/en': { lang: 'en' },
+  '': { lang: 'en' },
   '/de': { lang: 'de' },
   '/es': { lang: 'es' },
   '/fr': { lang: 'fr' },
