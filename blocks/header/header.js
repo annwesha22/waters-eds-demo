231db05d0c54aa1c07662c1c89a587e17d126625
Annwesha Bhattacharjee <conabha1@partner.waters.com>
2026-06-03 13:37:45 +0530

    commit four

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -115,50 +115,6 @@ function decorateMenu() {
   return null;
 }
 
-/**
- * Creates Categories dropdown menu
- * @param {Array} categories - Array of category names
- * @returns {HTMLElement}
- */
-/*function createCategoriesDropdown(categories = []) {
-  const wrapper = document.createElement('div');
-  wrapper.classList.add('categories-dropdown');
-
-  const button = document.createElement('button');
-  button.classList.add('categories-toggle');
-  button.innerHTML = `
-    Categories
-    <span class="dropdown-icon">⌄</span>
-  `;
-
-  const menu = document.createElement('ul');
-  menu.classList.add('categories-menu');
-
-  categories.forEach((category) => {
-    const item = document.createElement('li');
-
-    const link = document.createElement('a');
-    link.href = '#';
-    link.textContent = category;
-
-    item.appendChild(link);
-    menu.appendChild(item);
-  });
-
-  button.addEventListener('click', (e) => {
-    e.stopPropagation();
-    menu.classList.toggle('open');
-  });
-
-  document.addEventListener('click', () => {
-    menu.classList.remove('open');
-  });
-
-  wrapper.append(button, menu);
-
-  return wrapper;
-}*/
-
 function decorateMegaMenu(li) {
   const menu = li.querySelector('.fragment-content');
   if (!menu) return null;
@@ -184,7 +140,6 @@ function decorateNavItem(li) {
 function decorateBrandSection(section) {
   section.classList.add('brand-section');
   const brandLink = section.querySelector('a');
-  console.log('Header brand link:', brandLink);
   const [, text] = brandLink.childNodes;
   const span = document.createElement('span');
   span.className = 'brand-text';
@@ -218,17 +173,6 @@ async function decorateHeader(fragment) {
   if (sections[0]) decorateBrandSection(sections[0]);
   if (sections[1]) decorateNavSection(sections[1]);
   if (sections[2]) decorateActionSection(sections[2]);
-  // const categories = [
-  // 'Clinical',
-  // 'ESG',
-  // 'Food & Environmental',
-  // 'Materials Science',
-  // 'Pharmaceutical',
-  // 'Technology',
-  //   ];
-
-  // const nav = document.querySelector('.nav-tools'); // your header container
-  // nav.append(createCategoriesDropdown(categories));
 
   for (const pattern of HEADER_ACTIONS) {
     decorateAction(fragment, pattern);
