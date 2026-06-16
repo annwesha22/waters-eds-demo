728120211c52b7cd23785912406363865edd6bce
Antara Singha <conasin1@partner.waters.com>
2026-06-17 00:49:55 +0530

    commit for updating article-list tagpage variant two

--- blocks/article-list/article-list.js
+++ blocks/article-list/article-list.js
@@ -18,6 +18,8 @@ function formatTag(tag) {
 }
 
 export default async function decorate(block) {
+    // eslint-disable-next-line no-console
+console.log(block.className);
   const isTagPage = block.classList.contains('tagpage');
 
   // eslint-disable-next-line no-console
