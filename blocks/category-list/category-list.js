2ef570a514c65cc917678a0bb05e48996731d234
srustikn02 <srustikn02@gmail.com>
2026-06-19 15:39:18 +0530

    removing the category-list title

--- blocks/category-list/category-list.js
+++ blocks/category-list/category-list.js
@@ -148,9 +148,6 @@ export default async function init(block) {
   });
 
   block.innerHTML = `
-    <div class="category-header">
-      <h1>${categorySlug.replace(/-/g, ' ')}</h1>
-    </div>
 
     <div class="category-list-wrapper">
       ${articles.map(renderCard).join('')}
